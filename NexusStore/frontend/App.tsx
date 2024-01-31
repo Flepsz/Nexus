972fe99a8ccca709690851803e6c5bb35c5fe0e3
Flepsz <luisfelipelfsp3@gmail.com>
2024-01-31 08:05:34 -0300

    :sparkles: feat: React query config and navigation

--- NexusStore/frontend/App.tsx
+++ NexusStore/frontend/App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppStateStatus, Platform } from "react-native";
+import { QueryClientProvider, focusManager } from "@tanstack/react-query";
+import queryClient from "./src/services/queryClient";
+import { useAppState } from "./src/hooks/useAppState";
+import { useOnlineManager } from "./src/hooks/useOnlineManager";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./src/navigation/rootNavigator";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function onAppStateChange(status: AppStateStatus) {
+	if (Platform.OS !== "web") {
+		focusManager.setFocused(status === "active");
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+	useOnlineManager();
+	useAppState(onAppStateChange);
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<NavigationContainer>
+				<RootNavigator />
+			</NavigationContainer>
+		</QueryClientProvider>
+	);
+}
+
--- NexusStore/frontend/src/services/api.ts
+++ NexusStore/frontend/src/services/api.ts
@@ -1,14 +1,15 @@
-import axios, { AxiosResponse } from "axios";
-import { ProductDescription, ProductsSearch } from "./typesApi";
+import axios from "axios";
+import { ProductDescription, ProductsSearch } from "../queries/types";
 
-const axiosInstance = axios.create({
+const api = axios.create({
 	baseURL: "https://api.mercadolibre.com/",
-
 });
+
+export default api;
 
 export async function getProducts(search: string) {
 	try {
-		const response = await axiosInstance.get(`sites/MLB/search?q=${search}`);
+		const response = await api.get(`sites/MLB/search?q=${search}`);
     const data: ProductsSearch = response.data
 		console.log(data)
 	} catch (err) {
@@ -18,7 +19,7 @@ export async function getProducts(search: string) {
 
 export async function getProductsDescription(itemId: string) {
 	try {
-		const response = await axiosInstance.get(`items/${itemId}/description`);
+		const response = await api.get(`items/${itemId}/description`);
     const data: ProductDescription = response.data
 		console.log(data)
 	} catch (err) {
